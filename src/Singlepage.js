3398d69f047b35f0df451a84ebcae67e6602ebd4
ErikaLFrost <erika.loman@gmail.com>
2020-04-27 16:23:40 +0200

    add Ingress and MainText

--- src/Singlepage.js
+++ src/Singlepage.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from './components/Header';
+import RelatedPages from './components/RelatedPages';
+import Ingress from './components/Ingress';
+import MainText from './components/MainText';
 
 const Wrapper = styled.div`
 width: 100%;
 height: 100%;
 font-family: Helvetica, Arial, Sans-Serif;
+`
+const TextWrapper = styled.div`
+display: flex;
+flex-flow: row wrap;
+width: 80%;
+margin: 0 auto;
+padding-top: 50px;
 `
 
 const SinglePage = () => {
@@ -13,6 +23,11 @@ const SinglePage = () => {
     return (
         <Wrapper>
             <Header/>
+            <TextWrapper>
+            <Ingress/>
+            <RelatedPages/>
+            <MainText/>
+            </TextWrapper>
         </Wrapper>
     )
 }
---
+++ src/components/RelatedPages.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+import arrowIcon from '../assets/icon-arrow-alt-circle-up.svg'
+import calendarIcon from '../assets/icon-calendar-check.svg'
+import cloneIcon from '../assets/icon-clone.svg'
+
+const StyledRelatedPages = styled.div`
+flex: 1 auto;
+order: 2;
+h3{
+    margin: 0;
+}
+`
+
+const List = styled.ul`
+list-style: none;
+li{
+    img{
+        
+    }
+    p{
+        
+    }
+
+}
+`
+
+
+
+const RelatedPages = () => {
+    return (
+        <StyledRelatedPages>
+        <h3>Relaterade sidor</h3>
+        <List>
+            <li><img src={arrowIcon}/><p>Aktuella sparräntor</p></li>
+            <li><img src={calendarIcon}/><p>Jämför din sparränta</p></li>
+            <li><img src={cloneIcon}/><p>Barnsparkonto</p></li>
+        </List>
+        </StyledRelatedPages>
+    )
+}
+
+export default RelatedPages;
