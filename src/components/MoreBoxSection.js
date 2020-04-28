8c695d40fe19fae5e2de042a45bf35e7ddf374f9
ErikaLFrost <erika.loman@gmail.com>
2020-04-28 11:29:12 +0200

    add responsive design to MoreBoxSection

--- src/components/MoreBoxSection.js
+++ src/components/MoreBoxSection.js
@@ -6,34 +6,71 @@ import skatteverketlogo from '../assets/logo-skatteverket.png';
 const BoxSectionWrapper = styled.div`
   flex-basis: 100%;
   display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
   position: relative;
   order: 4;
-  height: 400px;
+  height: 40vw;
   background-color: rgb(242, 242, 242);
+  @media (max-width: 720px) {
+    flex-direction: column;
+    height: auto;
+  }
 `;
 
 const BoxOne = styled.div`
-  height: 75%;
-  width: 40%;
+  height: 30vw;
+  width: 35vw;
   background-color: white;
-  margin-right: 20px;
+  margin-right: 10vw;
   img {
     width: 60%;
-    padding-top: 15px;
+    padding-top: 30px;
+    margin: 0 auto;
   }
   p {
-    padding: 0 10px;
+    font-size: 1.2vw;
+    /* line-height: 1.5; */
+    padding: 20px 4vw 4.5vw;
+  }
+  button {
+    background-color: rgb(0, 153, 119);
+    color: white;
+    border: none;
+    border-radius: 50px;
+    font-size: 1vw;
+    font-weight: bold;
+    text-transform: uppercase;
+    padding: 5px 3vw;
+  }
+  @media (max-width: 720px) {
+    height: 50vw;
+    width: 55vw;
+    margin-top: 20px;
+    margin-right: 0;
+    p {
+      font-size: 2vw;
+      padding-top: 15px;
+    }
+    button {
+      font-size: 2vw;
+    }
   }
 `;
+
 const BoxTwo = styled.div`
-  height: 75%;
-  width: 40%;
+  height: 30vw;
+  width: 35vw;
   background: url(${illustration}) no-repeat;
   background-size: contain;
   background-position: center;
   background-color: rgb(0, 153, 119);
+  @media (max-width: 720px) {
+    height: 50vw;
+    width: 55vw;
+    margin: 20px 0;
+  }
 `;
 
 const MoreBoxSection = () => {
@@ -46,6 +83,7 @@ const MoreBoxSection = () => {
           och anmäl ditt konto redan idag så får du in skatteåterbäringen direkt
           på ditt sparkonto.
         </p>
+        <button>Läs mer</button>
       </BoxOne>
       <BoxTwo></BoxTwo>
     </BoxSectionWrapper>
